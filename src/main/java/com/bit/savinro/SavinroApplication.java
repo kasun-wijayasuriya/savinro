c6022ade8500409fea77d6d65a13811181c67cbe
kasun <kasunnalindaw@gmail.com>
2020-09-19 22:30:52 +0530

    Initial commit

---
+++ src/main/java/com/bit/savinro/SavinroApplication.java
@@ -0,0 +1,13 @@
+package com.bit.savinro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavinroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavinroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bit/savinro/SavinroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bit.savinro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavinroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
